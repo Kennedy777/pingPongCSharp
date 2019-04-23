7abbd3e1e4338cfc784cfdfdbd1086af5bf4427e
Marguerite Kennedy and Zema Kebede <marguerite@>
2019-04-23 13:38:57 -0700

    Finish logic

--- PingPong/Game.cs
+++ PingPong/Game.cs
@@ -1,20 +1,35 @@
 using System;
-
 using System.Collections.Generic;
 
 namespace PingPong
 {
-  public class Game
+   class Game
   {
     public static void Main()
     {
-      List<int> Numbers = new List<int>() {};
       Console.WriteLine("Let's play PingPong, what's your favorite number?");
+      string initialString = Console.ReadLine();
+      int inputNumber = int.Parse(initialString);
 
-      int inputNumber = int.Parse(Console.ReadLine());
-      Numbers.Add(inputNumber);
-      Console.WriteLine("Your Number is: "+ Numbers[0]);
-
+       for(int i=1; i<=inputNumber; i++)
+        {
+          if (i%5==0 && i%3==0)
+          {
+          Console.WriteLine("ping-pong");
+          }
+          else if (i%5 ==0)
+          {
+            Console.WriteLine("pong");
+          }
+          else if (i%3 == 0)
+          {
+            Console.WriteLine("ping");
+          }
+          else
+          {
+          Console.WriteLine(i);
+          }
+        }
     }
   }
 }
