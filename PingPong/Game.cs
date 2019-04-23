b9a72842b344a216cf777167470e38e82bcae453
Marguerite Kennedy and Zema Kebede <zema@>
2019-04-23 11:54:51 -0700

    Program outputs entered number

--- PingPong/Game.cs
+++ PingPong/Game.cs
@@ -1,10 +1,20 @@
+using System;
+
+using System.Collections.Generic;
+
 namespace PingPong
 {
   public class Game
   {
     public static void Main()
     {
+      List<int> Numbers = new List<int>() {};
+      Console.WriteLine("Let's play PingPong, what's your favorite number?");
+
+      int inputNumber = int.Parse(Console.ReadLine());
+      Numbers.Add(inputNumber);
+      Console.WriteLine("Your Number is: "+ Numbers[0]);
 
     }
-  }  
+  }
 }
---
+++ PingPong/notes.md
@@ -0,0 +1,6 @@
+_Specs_
+
+1. Program returns a list of numbers from one to the inputted number
+2. Program replaces all numbers divisible by 15 with "ping-pong"
+3. Program replaces all numbers divisible by 5 with "pong"
+4. Program replaces all numbers divisible by 3 with "ping"
